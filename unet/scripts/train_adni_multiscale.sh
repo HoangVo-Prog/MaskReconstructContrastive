b116c8f459efe3b89cbf6de17939a9870ac6062f
Hoang Vo~ <vohoang2204@gmail.com>
2025-11-20 14:31:41 +0700

    Debug adni scripts

--- unet/scripts/train_adni_multiscale.sh
+++ unet/scripts/train_adni_multiscale.sh
@@ -6,4 +6,4 @@ IMAGE_TYPE=$3  # axial or coronal
 python unet/src/"$VER"/train.py --amp \
   --use-gn --use-se --use-multiscale \
   --pre-bias --pre-norm --pre-crop --pre-align \
-  --adni --adni-path "$ADNI_PATH" --image-type "$IMAGE_TYPE"
+  --adni True --adni-path "$ADNI_PATH" --image-type "$IMAGE_TYPE"
