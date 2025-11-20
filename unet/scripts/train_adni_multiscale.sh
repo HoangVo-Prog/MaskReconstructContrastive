3a491820a7d695791f839ad695cac1fa9f7c503b
Hoang Vo~ <vohoang2204@gmail.com>
2025-11-20 14:40:27 +0700

    Add epochs parameters

--- unet/scripts/train_adni_multiscale.sh
+++ unet/scripts/train_adni_multiscale.sh
@@ -2,8 +2,9 @@
 VER=$1
 ADNI_PATH=$2
 IMAGE_TYPE=$3  # axial or coronal
+EPOCH=$4
 
 python unet/src/"$VER"/train.py --amp \
   --use-gn --use-se --use-multiscale \
   --pre-bias --pre-norm --pre-crop --pre-align \
-  --adni True --adni-path "$ADNI_PATH" --image-type "$IMAGE_TYPE"
+  --adni True --adni-path "$ADNI_PATH" --image-type "$IMAGE_TYPE" --epochs "$EPOCH" 
