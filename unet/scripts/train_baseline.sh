13e726c2c2a11ecd1264f920c2e47917f78e6f7f
Hoang Vo~ <vohoang2204@gmail.com>
2025-11-21 15:24:59 +0700

    Add adni scripts and remove stablity results

---
+++ unet/scripts/train_baseline.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+VER=$1
+
+if [ "$USE_MASKED_LOSS" = "true" ]; then
+  MASKED_FLAG="--enable-masked-loss"
+else
+  MASKED_FLAG=""
+fi
+
+python unet/src/"$VER"/train.py --amp \
+  --use-gn --use-se --use-multiscale \
+  --pre-bias --pre-norm --pre-crop --pre-align \
+  $MASKED_FLAG
